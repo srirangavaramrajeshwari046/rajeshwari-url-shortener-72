bed66f5037f68daddaa739c7248b75bfcdb9406a
rajeshwari <srrangavaramrajeshwari046@gmail.com>
2025-09-06 11:29:10 +0530

    chnaged home page

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+           rajeshwari url shortner
           </Title>
 
           <Text
